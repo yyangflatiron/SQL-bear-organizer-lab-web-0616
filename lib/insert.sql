5fac99d982b25cfc76d5c00db11c6dfd90e9701c
yyangflatiron <yichen.yang@flatironschool.com>
2016-06-17 11:59:04 -0400

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Mr.Chocolate', 10,'M','Brown','Calm',1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Rowdy', 3,'M','Black','Intense',1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Tabitha', 11,'M','White','Nice',1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Sergeant Brown', 10,'M','Beige','Goofy',1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Melissa', 2,'M','Brown','Naive',1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Grinch', 7,'M','Beige','Nice',1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Wendy', 8,'M','Black','Aggressive',1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (NULL, 6,'M','Yellow','Aggressive',0);
